2c70109d41350667800914e7beea6a249ea9fd96
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-08-26 10:45:28 -0300

    Verificacao de status / 26-08-25 as 10:45

--- app_main.py
+++ app_main.py
@@ -6,12 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
 
-class Login:
 
+class Login:
     def __init__(self):
         self.url = "http://ead.cecp.sp.gov.br/login/index.php"
         self.driver = None
-        
+
     def setup_driver(self):
         try:
             options = Options()
@@ -30,15 +30,17 @@ class Login:
             try:
                 ping = requests.get(self.url)
                 if ping.status_code == 200:
-                    return print(f'Site ON as {self.now}')
+                    return print(f"Site ON as {self.now}")
             except:
-                print(f'Site OFF as {self.now}')
+                print(f"Site OFF as {self.now}")
             time.sleep(10)
-        
+
     def navigate(self):
         if not self.driver:
-            raise Exception("Driver não foi inicializado. Chame setup_driver() primeiro.")
-        
+            raise Exception(
+                "Driver não foi inicializado. Chame setup_driver() primeiro."
+            )
+
         try:
             self.driver.get(self.url)
             self.wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
@@ -46,60 +48,59 @@ class Login:
         except Exception as e:
             print(f"Erro na navegação: {e}")
             raise
-        
-        
+
     def find_login_elements(self):
         username = self.driver.find_element(By.XPATH, "//*[@id='username']")
-        password = self.driver.find_element(By.XPATH, "//*[@id=\"password\"]")
-        login_btn = self.driver.find_element(By.XPATH, "//*[@id=\"loginbtn\"]")
-        
+        password = self.driver.find_element(By.XPATH, '//*[@id="password"]')
+        login_btn = self.driver.find_element(By.XPATH, '//*[@id="loginbtn"]')
+
         return username, password, login_btn
-        
+
     def login(self, user, pwd):
         username, password, login_btn = self.find_login_elements()
-        
+
         username.send_keys(user)
         password.send_keys(pwd)
         login_btn.click()
         time.sleep(5)
-        
-    def get_cursos(self):
-        self.driver.find_element(By.XPATH, "/html/body/div[3]/nav/div[1]/nav/ul/li[3]/a").click()
-        time.sleep(5)
-        self.driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div[1]/div[2]/div/section/div/section/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[1]/div/a").click()
-        #self.driver.find_element(By.XPATH, "/html/body/div[3]/nav/div[1]/nav/ul/li[3]/ul/li[1]/a").click()
-        
-    def processar_cursos(self):
-        self.driver.find_element(By.XPATH, "/html/body/div[3]/div[5]/div[1]/div[3]/div/section/div/div[1]/div/ul")
 
-        
-    #def close(self):
+    def get_cursos(self):
+        self.driver.find_element(
+            By.XPATH, "/html/body/div[3]/nav/div[1]/nav/ul/li[3]/a"
+        ).click()
+        time.sleep(5)
+        self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[3]/div[3]/div[1]/div[2]/div/section/div/section/section/div/div/div[1]/div[2]/div/div/div[1]/div/div/div[1]/div/a",
+        ).click()
+        # self.driver.find_element(By.XPATH, "/html/body/div[3]/nav/div[1]/nav/ul/li[3]/ul/li[1]/a").click()
+
+    def processar_cursos(self):
+        self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[3]/div[5]/div[1]/div[3]/div/section/div/div[1]/div/ul",
+        )
+
+    # def close(self):
     #    if self.driver:
     #        self.driver.quit()
 
+
 def main():
     login = Login()
-    
-    try:
+    if login.ping() == 200:
         print("Configurando driver...")
         login.setup_driver()
-        
         print("Navegando para o site...")
         login.navigate()
-        
         print("Fazendo login...")
-        login.login('370.768.438-54','@Leon02023091')
-        
+        login.login("370.768.438-54", "@Leon02023091")
         print("Acessando cursos...")
         login.get_cursos()
         time.sleep(30)
+    else:
+        login.ping()
 
-    except Exception as e:
-        print(f"Erro: {e}")
-    finally:
-        if login.driver:
-            login.driver.quit()
-        #scraper.close()
 
 if __name__ == "__main__":
     main()
